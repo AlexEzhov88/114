d83f3f165f3f37c0c65a01e3ca4afd5bb342972b
AleksandrEzhov <104377724+AlexEzhov88@users.noreply.github.com>
2022-11-12 19:01:44 +0300

    Add files via upload

---
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package jm.task.core.jdbc.service;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDaoJDBC.createUsersTable();
+        System.out.println("Таблица создана");
+    }
+
+    public void dropUsersTable() {
+        userDaoJDBC.dropUsersTable();
+        System.out.println("Таблица удалена");
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        userDaoJDBC.saveUser(name, lastName, age);
+        System.out.println("User с именем – " + name + " добавлен в базу данных");
+    }
+
+    public void removeUserById(long id) {
+        userDaoJDBC.removeUserById(id);
+    }
+
+    public List<User> getAllUsers() {
+        System.out.println(userDaoJDBC.getAllUsers());
+        return userDaoJDBC.getAllUsers();
+    }
+
+    public void cleanUsersTable() {
+        userDaoJDBC.cleanUsersTable();
+        System.out.println("Таблица очищена");
+    }
+}
